f6a13649675272292a3302150c1049f6f7eec244
Xujaniyozov <akrom2661@gmail.com>
2025-02-23 16:35:37 +0500

    initilizing foodies app using Next.js

---
+++ app/meals/[slug]/page.jsx
@@ -0,0 +1,7 @@
+export default function Somethings() {
+  return (
+    <div>
+      <h1>Somethings</h1>
+    </div>
+  );
+}
---
+++ app/meals/share/page.jsx
@@ -0,0 +1,7 @@
+export default function Share() {
+  return (
+    <div>
+      <h1>Share Meals</h1>
+    </div>
+  );
+}
