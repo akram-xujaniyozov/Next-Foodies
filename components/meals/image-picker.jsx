41eb2a29fe165035601028717f64acbe3d97e9e9
Xujaniyozov <akrom2661@gmail.com>
2025-04-24 01:27:12 +0500

    add new features image prewiev feature

--- components/meals/image-picker.jsx
+++ components/meals/image-picker.jsx
@@ -1,19 +1,48 @@
 "use client";
 
-import { useRef } from "react";
+import { useRef, useState } from "react";
+import Image from "next/image";
+
 import classes from "./image-picker.module.css";
 
 export default function ImagePicker({ label, name }) {
+  const [pickedImage, setPickedImage] = useState();
   const imageInput = useRef();
 
   function handlePickClick() {
     imageInput.current.click();
   }
 
+  const handleImageChange = (event) => {
+    const file = event.target.files[0];
+
+    if (!file) {
+      setPickedImage(null);
+      return;
+    }
+
+    const fileReader = new FileReader();
+    fileReader.onload = () => {
+      setPickedImage(fileReader.result);
+    };
+    fileReader.readAsDataURL(file);
+  };
+
   return (
     <div className={classes.picker}>
       <label htmlFor={name}>{label}</label>
       <div className={classes.controls}>
+        <div className={classes.preview}>
+          {!pickedImage && <p>No picked image yet</p>}
+          {pickedImage && (
+            <Image
+              src={decodeURIComponent(pickedImage)}
+              blurDataURL={pickedImage}
+              alt="The image selected by user"
+              fill
+            />
+          )}
+        </div>
         <input
           className={classes.input}
           type="file"
@@ -21,6 +50,7 @@ export default function ImagePicker({ label, name }) {
           accept="image/png, image/jpeg"
           name={name}
           ref={imageInput}
+          onChange={handleImageChange}
         />
         <button
           className={classes.button}
